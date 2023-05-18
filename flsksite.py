f447e81b751faf2407f07b4dc3dabf5ac588f4d8
Ulyana Nesterova <ulnesterova@outlook.com>
2023-05-18 16:43:53 +0300

    url_for function

--- flsksite.py
+++ flsksite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,11 +6,22 @@ menu = ["Установка", "Первое приложение", "Обратн
 
 @app.route("/")
 def index():
+    print(url_for(('index')))
     return render_template('index.html', title="Про Flask", menu=menu)
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title = "О сайте", menu = menu)
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+#with app.test_request_context():
+#    print(url_for('index'))
+#    print(url_for('about'))
+#    print(url_for('profile', username="selfedu"))
 
 if __name__ == "__main__":
     app.run(debug=True)
