47eb4ce489401a6bdb084a404f2ef1f127e6283d
Ulyana Nesterova <ulnesterova@outlook.com>
2023-05-18 20:07:38 +0300

    url_for function

--- flsksite.py
+++ flsksite.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 @app.route("/")
 def index():
@@ -18,10 +20,16 @@ def about():
 def profile(username):
     return f"Пользователь: {username}"
 
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+
+    return render_template('contact.html', title="Обратная связь", menu=menu)
 #with app.test_request_context():
 #    print(url_for('index'))
 #    print(url_for('about'))
-#    print(url_for('profile', username="selfedu"))
+#    print(url_for('profile', username="ivanov"))
 
 if __name__ == "__main__":
     app.run(debug=True)
