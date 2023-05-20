9617788abef8fe53a432f4c4e5206b32b72e63d4
Ulyana Nesterova <ulnesterova@outlook.com>
2023-05-20 15:22:47 +0300

    flask#7

--- flsksite.py
+++ flsksite.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect, session, abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -16,16 +17,36 @@ def about():
     print(url_for('about'))
     return render_template('about.html', title = "О сайте", menu = menu)
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"Пользователь: {username}"
-
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
-        print(request.form)
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
 
     return render_template('contact.html', title="Обратная связь", menu=menu)
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"Пользователь: {username}"
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "uliana" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
 #with app.test_request_context():
 #    print(url_for('index'))
 #    print(url_for('about'))
